fc5928eec5bfb7caa9d2891227927aeffed8e272
Pedro Reina Ganan <preina-g@c2r2s1.42malaga.com>
2024-03-17 11:34:25 +0100

    quiero sacar esto ya porfaaa

--- srcs/requirements/nginx/tools/config_nginx.sh
+++ srcs/requirements/nginx/tools/config_nginx.sh
@@ -4,3 +4,4 @@
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/preina-g.key -out /etc/ssl/certs/preina-g.crt -subj "/C=SP/ST=Malaga/L=Malaga/O=42 Malaga/OU=preina-g/CN=preina-g"
 
+nginx -g "daemon off;"
--- srcs/requirements/wordpress/tools/install_wp.sh
+++ srcs/requirements/wordpress/tools/install_wp.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-#check if wp-config.php exists
-if [ -f ./wp-config.php]
+WP_CONFIG_FILE="/var/www/wordpress/wp-config.php"
+WP_CONFIG_SAMPLE="/var/www/wordpress/wp-config-sample.php"
+WP_CLI_URL="https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
+
+#check if wp-config.php exist
+if [ -f ./wp-config.php ]
 then
-    echo "wordpress already downladed"
+	echo "wordpress already downloaded"
 else
 
+####### MANDATORY PART ##########
 
-## Download and config wordpress
+	#Download wordpress and all config file
+	wget http://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
+	mv wordpress/* .
+	rm -rf latest.tar.gz
+	rm -rf wordpress
 
-wget http://wordpress.org/latest.tar.gz #download wp
-tar xfz latest.tar.gz #decompress the file downloaded
-mv wordpress/* .
-rm -rf latest.tar.gz
-rm -rf wordpress
-##import env variables in conf file
+	#Inport env variables in the config file
+	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	cp wp-config-sample.php wp-config.php
 fi
 
-/usr/sbin/php-fpm7.3 -F
+/usr/sbin/php-fpm7.4 -y /etc/php/7.4/fpm/php-fpm.conf -F
